4ac04f855c2163e048172b1f9a78c4eb2d87f5b4
h1m4n3hu <himanshusoni2708@gmail.com>
2021-03-08 16:41:29 +0530

    Updates

---
+++ precache-manifest.b3cb0ebe66e78a3cd99ad74d872d1f5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "019f4edbbab5d47e3c952c12a6a88f0f",
+    "url": "/reakt/index.html"
+  },
+  {
+    "revision": "580693d85ebd0444b746",
+    "url": "/reakt/static/css/main.6942973f.chunk.css"
+  },
+  {
+    "revision": "77130e1cd3df50dbe03b",
+    "url": "/reakt/static/js/2.ddcd3477.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reakt/static/js/2.ddcd3477.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "580693d85ebd0444b746",
+    "url": "/reakt/static/js/main.0f087442.chunk.js"
+  },
+  {
+    "revision": "62573d461a7a3a30cb96",
+    "url": "/reakt/static/js/runtime-main.ddcc5db6.js"
+  }
+]);
