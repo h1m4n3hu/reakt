d483259523195d8b957d552c6397c2de0ee4e9f3
h1m4n3hu <himanshusoni2708@gmail.com>
2021-03-08 16:26:57 +0530

    my

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Pager from "./One"
+
+const Nav=()=>{
+    return(
+        <Pager/>
+    )
+}
+
+export default Nav
---
+++ src/Hack.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+var wifi = require("node-wifi");
+console.log("object")
+wifi.init()
+console.log("22222");
+
+const Wif=()=>{
+    return(
+        <h1>aaj pakka</h1>
+    )
+}
+
+export default Wif
